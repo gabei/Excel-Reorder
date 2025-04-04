8c9d306936b504f61705f063098469fe8d7d8f97
gabei <gabe1792@gmail.com>
2025-04-04 15:45:57 -0500

    Chore: Remove old xlsx processer and add ExcelJS. Start toying with data

--- index.js
+++ index.js
@@ -13,3 +13,24 @@ Vendor 	PO 	Received	Fund Code	Pub Date	Top Note	Title	Sub-Title	Author	Qty	List
 
 
 
+const ExcelJS = require('exceljs');
+
+const requiredColumns = ["Vendor", "PO Received", "Fund", "Code",	"Pub Date", "Top Note", "Title",	"Sub-Title", "Author", "Qty",	"List Price",	"Unit Price",	"Line Price"];
+
+const resultingColumns = [];
+
+async function readInFile(){
+  const workbook = new ExcelJS.Workbook();
+  const worksheet = await workbook.csv.readFile('./input/input.csv');
+
+  worksheet.columns.forEach(column => {
+    const thisColumnTitle = column.values[1];
+
+    if( requiredColumns.includes(thisColumnTitle) ) {
+      resultingColumns.push(column);
+    }
+
+  });
+}
+
+readInFile();

8bf79c94717735f4601f18c62fe0f20055719f12
gabei <gabe1792@gmail.com>
2025-04-04 11:08:22 -0500

    Chore: Add quick note comment to describe problem

---
+++ index.js
@@ -0,0 +1,15 @@
+/* 
+
+- Read in excel file from input folder
+- Remove extra columns
+- Reorder columns
+- Save manipulated sheet to output folder
+
+
+- Columns to keep (in this order):
+Vendor 	PO 	Received	Fund Code	Pub Date	Top Note	Title	Sub-Title	Author	Qty	List Price	Unit Price	Line Price													
+
+*/
+
+
+
